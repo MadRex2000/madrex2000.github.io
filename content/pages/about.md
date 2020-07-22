4861d1aa52c430b92b6999af595cc28da16e907e
MadRex2000 <spplkk113@gmail.com>
2020-07-23 05:19:41 +0800

    first commit

---
+++ content/pages/about.md
@@ -0,0 +1,5 @@
+Title: About me
+
+這裡還沒有內容 Orz
+
+等哪天心血來潮有靈感再補上XD
---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Rex'
+SITENAME = "Rex's Blog"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Taipei'
+
+DEFAULT_LANG = 'zh'
+
+THEME = 'pelican-blueidea'
+
+FACEBOOK_USER = 'RexWu1123'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Fishing Map', 'https://fishingmap.site'),)
+
+# Social widget
+SOCIAL = (('Facebook', 'https://www.facebook.com/RexWu1123'),
+          ('Github', 'https://github.com/MadRex2000'),
+          ('Linkedin', 'https://www.linkedin.com/in/%E5%BF%B5%E4%BF%AE-%E5%90%B3-b28a48160/'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
